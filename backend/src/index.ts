350cf6b084128c798aa9297760927f6f1ba10f66
Rick Pratihar <103872207+RickPratihar@users.noreply.github.com>
2024-05-07 11:31:31 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,13 +19,16 @@ cloudinary.config({
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
 
+const isProduction = process.env.NODE_ENV === 'production';
+
+
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: https://mern-hotel-booking-umqw.onrender.com,
+    origin: isProduction ? process.env.FRONTEND_URL : 'http://localhost:5173',
     credentials: true,
   })
 );
