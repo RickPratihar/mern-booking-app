ec461c8755e3a0f5762bac7301d26045c0027764
Rick Pratihar <rickpratihar1200@gmail.com>
2026-06-07 21:04:44 +0530

    fix build error

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -3,7 +3,6 @@ import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { Link, useNavigate } from "react-router-dom";
-import Header from "../components/Header";
 
 export type RegisterFormData = {
   firstName: string;
--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,7 +3,6 @@ import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import Header from "../components/Header";
 
 export type SignInFormData = {
   email: string;
